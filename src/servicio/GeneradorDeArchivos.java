dac3a5a21bd82814889e0b19465f0dddd0d095af
gonzalo <gonzalo>
2024-06-26 12:52:35 -0300

    generador de archivos documentacion

--- src/servicio/GeneradorDeArchivos.java
+++ src/servicio/GeneradorDeArchivos.java
@@ -8,20 +8,48 @@ import modelos.Temperatura;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * Clase para generar archivos JSON a partir de consultas de moneda y temperatura.
+ */
 public class GeneradorDeArchivos {
+
+    /**
+     * Guarda la consulta de una moneda en un archivo JSON.
+     *
+     * @param moneda Objeto de tipo Moneda a guardar en JSON.
+     * @throws IOException Si ocurre un error al escribir el archivo.
+     */
     public void guardarConsultaMoneda(Moneda moneda) throws IOException {
+        // Configurar Gson para formatear el JSON de manera legible
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        FileWriter escritura = new FileWriter(moneda.base_code()+".json");
+        // Crear FileWriter para escribir en el archivo con el nombre basado en el código base de la moneda
+        FileWriter escritura = new FileWriter(moneda.base_code() + ".json");
+
+        // Convertir el objeto Moneda a JSON y escribirlo en el archivo
         escritura.write(gson.toJson(moneda));
+
+        // Cerrar FileWriter después de escribir
         escritura.close();
     }
 
+    /**
+     * Guarda la consulta de una temperatura en un archivo JSON.
+     *
+     * @param temperatura Objeto de tipo Temperatura a guardar en JSON.
+     * @throws IOException Si ocurre un error al escribir el archivo.
+     */
     public void guardarConsultaTemperatura(Temperatura temperatura) throws IOException {
+        // Configurar Gson para formatear el JSON de manera legible
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        FileWriter escritura = new FileWriter(temperatura.toString()+".json");
+        // Crear FileWriter para escribir en el archivo con el nombre basado en la representación textual de la temperatura
+        FileWriter escritura = new FileWriter(temperatura.toString() + ".json");
+
+        // Convertir el objeto Temperatura a JSON y escribirlo en el archivo
         escritura.write(gson.toJson(temperatura));
+
+        // Cerrar FileWriter después de escribir
         escritura.close();
     }
 }
